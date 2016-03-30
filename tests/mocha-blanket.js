0b3a1b2b738e7aefe8e66c711e0944736219cb0a
alu0100698688 <alu0100698688@ull.edu.es>
2016-03-31 00:42:12 +0100

    Intentando solucionar problema Travis

--- tests/mocha-blanket.js
+++ tests/mocha-blanket.js
@@ -1,6 +1,6 @@
 (function() {
 
-    if(!mocha) {
+    if (!mocha) {
         throw new Exception("mocha library does not exist in global namespace!");
     }
 
@@ -24,37 +24,29 @@
     var OriginalReporter = mocha._reporter;
 
     var BlanketReporter = function(runner) {
-            runner.on('start', function() {
-                blanket.setupCoverage();
-            });
+        runner.on('start', function() {
+            blanket.setupCoverage();
+        });
 
-            runner.on('end', function() {
-                blanket.onTestsDone();
-            });
+        runner.on('end', function() {
+            blanket.onTestsDone();
+        });
 
-            runner.on('suite', function() {
-                blanket.onModuleStart();
-            });
+        runner.on('suite', function() {
+            blanket.onModuleStart();
+        });
 
-            runner.on('test', function() {
-                blanket.onTestStart();
-            });
+        runner.on('test', function() {
+            blanket.onTestStart();
+        });
 
-            runner.on('test end', function(test) {
-                blanket.onTestDone(test.parent.tests.length, test.state === 'passed');
-            });
+        runner.on('test end', function(test) {
+            blanket.onTestDone(test.parent.tests.length, test.state === 'passed');
+        });
 
-            runner.on('hook', function(){
-                blanket.onTestStart();
-            });
-
-            runner.on('hook end', function(){
-                blanket.onTestsDone();
-            });
-
-            // NOTE: this is an instance of BlanketReporter
-            new OriginalReporter(runner);
-        };
+        // NOTE: this is an instance of BlanketReporter
+        new OriginalReporter(runner);
+    };
 
     BlanketReporter.prototype = OriginalReporter.prototype;
 
@@ -63,13 +55,13 @@
     var oldRun = mocha.run,
         oldCallback = null;
 
-    mocha.run = function (finishCallback) {
-      oldCallback = finishCallback;
-      console.log("waiting for blanket...");
+    mocha.run = function(finishCallback) {
+        oldCallback = finishCallback;
+        console.log("waiting for blanket...");
     };
     blanket.beforeStartTestRunner({
-        callback: function(){
-            if (!blanket.options("existingRequireJS")){
+        callback: function() {
+            if (!blanket.options("existingRequireJS")) {
                 oldRun(oldCallback);
             }
             mocha.run = oldRun;
